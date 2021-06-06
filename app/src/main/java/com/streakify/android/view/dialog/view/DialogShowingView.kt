5ad62e5bd39cff4a285fc631652cfa1fad538546
Amit Sharma <amitsharmamail101@gmail.com>
2021-06-06 12:55:24 +0530

    added dialogs

--- app/src/main/java/com/streakify/android/view/dialog/view/DialogShowingView.kt
+++ app/src/main/java/com/streakify/android/view/dialog/view/DialogShowingView.kt
@@ -1,38 +1,48 @@
 package com.streakify.android.view.dialog.view
 
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.AttrRes
+import androidx.databinding.BindingAdapter
+import com.streakify.android.view.dialog.IDialogViewModel
+import com.streakify.android.view.dialog.common.DialogUiConfig
 
-//class DialogShowingView @JvmOverloads constructor(
-//    context: Context,
-//    attrs: AttributeSet? = null,
-//    @AttrRes defStyleAttr: Int = 0
-//) : BaseShowingView(context, attrs, defStyleAttr) {
-//
-//    var bindingData: Pair<DialogUiConfig?, IDialogViewModel?>? = null
-//        set(value) {
-//            field = value
-//            value?.let { (config, viewModel) ->
-//                binding.setVariable(BR.uiConfig, config)
-//                binding.setVariable(BR.viewModel, viewModel)
-//            }
-//        }
-//}
-//
-//@BindingAdapter(value = ["dialogConfig", "dialogViewModel"], requireAll = false)
-//fun DialogShowingView.bindTextAndActions(
-//    dialogConfig: DialogUiConfig? = null,
-//    dialogViewModel: IDialogViewModel? = null
-//) {
-//
-//    cancelable = dialogConfig?.cancelable ?: false
-//
-//    dialog.setCancelable(cancelable)
-//
-//    bindingData = Pair(dialogConfig, dialogViewModel)
-//}
-//
-//fun DialogShowingView.show(
-//    show: Boolean? = null,
-//) {
-//    if (show != null)
-//        visibility = if (show) View.VISIBLE else View.GONE
-//}
+import com.streakify.android.BR
+
+
+class DialogShowingView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0
+) : BaseShowingView(context, attrs, defStyleAttr) {
+
+    var bindingData: Pair<DialogUiConfig?, IDialogViewModel?>? = null
+        set(value) {
+            field = value
+            value?.let { (config, viewModel) ->
+                binding.setVariable(BR.uiConfig, config)
+                binding.setVariable(BR.viewModel, viewModel)
+            }
+        }
+}
+
+@BindingAdapter(value = ["dialogConfig", "dialogViewModel"], requireAll = false)
+fun DialogShowingView.bindTextAndActions(
+    dialogConfig: DialogUiConfig? = null,
+    dialogViewModel: IDialogViewModel? = null
+) {
+
+    cancelable = dialogConfig?.cancelable ?: false
+
+    dialog.setCancelable(cancelable)
+
+    bindingData = Pair(dialogConfig, dialogViewModel)
+}
+
+fun DialogShowingView.show(
+    show: Boolean? = null,
+) {
+    if (show != null)
+        visibility = if (show) View.VISIBLE else View.GONE
+}
--- app/src/main/java/com/streakify/android/view/dialog/view/LoadingShowingView.kt
+++ app/src/main/java/com/streakify/android/view/dialog/view/LoadingShowingView.kt
@@ -1,27 +1,33 @@
 package com.streakify.android.view.dialog.view
 
-//import com.example.streakify.view.dialog.common.LoadingUiConfig
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import androidx.annotation.AttrRes
+import androidx.databinding.BindingAdapter
+import com.streakify.android.view.dialog.common.LoadingUiConfig
+import com.streakify.android.BR
 
-//
-//@SuppressLint("Recycle")
-//class LoadingShowingView @JvmOverloads constructor(
-//    context: Context,
-//    attrs: AttributeSet? = null,
-//    @AttrRes defStyleAttr: Int = 0
-//) : BaseShowingView(context, attrs, defStyleAttr) {
-//
-//    var bindingData: LoadingUiConfig? = null
-//        set(value) {
-//            field = value
-//            value?.let {
-//                binding.setVariable(BR.uiConfig, it)
-//            }
-//        }
-//}
-//
-//@BindingAdapter(value = ["loadingDialogConfig"], requireAll = false)
-//fun LoadingShowingView.bindTextAndActions(
-//    dialogConfig: LoadingUiConfig? = null
-//) {
-//    bindingData = dialogConfig
-//}
+
+@SuppressLint("Recycle")
+class LoadingShowingView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0
+) : BaseShowingView(context, attrs, defStyleAttr) {
+
+    var bindingData: LoadingUiConfig? = null
+        set(value) {
+            field = value
+            value?.let {
+                binding.setVariable(BR.uiConfig, it)
+            }
+        }
+}
+
+@BindingAdapter(value = ["loadingDialogConfig"], requireAll = false)
+fun LoadingShowingView.bindTextAndActions(
+    dialogConfig: LoadingUiConfig? = null
+) {
+    bindingData = dialogConfig
+}
